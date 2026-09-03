f45c15d7924461f286a7138080a867be3c3b7c6b
Esteban <esteban.horacio.acevedo@gmail.com>
2026-09-03 19:54:14 -0300

    Ejercicio 1

--- src/core/views.py
+++ src/core/views.py
@@ -1,2 +1,10 @@
+from datetime import UTC, datetime
 
-# Create your views here.
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    mensaje = input("Dime tu mensaje: ")
+    contexto = {"titulo": "Servicios", "mensaje": mensaje, "fecha": datetime.now(UTC)}
+    return render(request, "core/index.html", contexto)
