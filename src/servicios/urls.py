b0561674fe7908921e1f10fe6c103864d39dc225
Esteban <esteban.horacio.acevedo@gmail.com>
2026-09-10 22:04:03 -0300

    wip

--- src/servicios/urls.py
+++ src/servicios/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from servicios.views import index
+from servicios.views import cliente_list, index, servicio_list
 
 app_name = "servicios"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("servicio/list", servicio_list, name="servicio_list"),
+    path("cliente/list", cliente_list, name="cliente_list"),
 ]
--- src/servicios/views.py
+++ src/servicios/views.py
@@ -1,5 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from servicios.models import Cliente, Servicio
 
-def index(request):
+
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "servicios/index.html")
+
+
+def servicio_list(request: HttpRequest) -> HttpResponse:
+    servicios = Servicio.objects.all()
+    return render(request, "servicios/servicio_list.html", {"servicios": servicios})
+
+
+def cliente_list(request: HttpRequest) -> HttpResponse:
+    clientes = Cliente.objects.all()
+    return render(request, "servicios/cliente_list.html", {"clientes": clientes})
